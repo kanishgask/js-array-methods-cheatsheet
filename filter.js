72df90446972af3e599902851af6ec4f5d223d0a
KANISHGA <168720129+kanishgask@users.noreply.github.com>
2026-03-29 07:33:37 +0530

    Create filter.js

---
+++ filter.js
@@ -0,0 +1,7 @@
+// filter() returns elements that satisfy a condition
+
+const numbers = [1,2,3,4,5,6];
+
+const evenNumbers = numbers.filter(num => num % 2 === 0);
+
+console.log("Even Numbers:", evenNumbers);
